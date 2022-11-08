cbbb29d843daa8dcc69c7537179b6ead033a4fe8
MhSolyman <solymanbijoye.com>
2022-11-09 01:12:51 +0600

    add slidr

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Slider from '../Slider/Slider';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>home</h1>
+            
+
+           <Slider>
+            
+           </Slider>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/Slider/Slider.js
@@ -0,0 +1,45 @@
+import { Carousel ,} from 'flowbite-react';
+import React from 'react';
+
+
+const Slider = () => {
+    return (
+        
+        
+          
+
+
+<div className="h-96 sm:h-64 xl:h-80 2xl:h-96 mt-0">
+  <Carousel>
+    <img 
+      src="https://images.unsplash.com/photo-1512152272829-e3139592d56f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+      alt="..."
+    />
+    <img
+      src="https://images.unsplash.com/photo-1630409346824-4f0e7b080087?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1246&q=80"
+      alt="..."
+    />
+    <img
+      src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      alt="..."
+    />
+    
+  </Carousel>
+</div>
+
+
+
+
+
+
+
+
+
+
+   
+            
+       
+    );
+};
+
+export default Slider;

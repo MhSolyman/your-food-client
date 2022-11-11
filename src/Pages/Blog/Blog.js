eb6c829a5c56d107eaa49439049e9741f8b2724c
MhSolyman <solymanbijoye.com>
2022-11-11 22:56:40 +0600

    ad 2nd q ans

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -8,6 +8,8 @@ const Blog = () => {
            (Relational databases model data as records in rows and tables with logical links between them). <br />
            NoSQL is a class of DBMs that are non-relational and generally do not use SQL.
            q: What is JWT, and how does it work?
+           ans: JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting <br />
+            information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.
         </div>
     );
 };

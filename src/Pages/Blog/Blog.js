48513fe11702b8cc2b52e389d7a8076aa1ddaab2
MhSolyman <solymanbijoye.com>
2022-11-11 22:55:18 +0600

    ad 2d q and ans 2

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -7,6 +7,7 @@ const Blog = () => {
           ans:SQL is the programming language used to interface with relational databases. <br />
            (Relational databases model data as records in rows and tables with logical links between them). <br />
            NoSQL is a class of DBMs that are non-relational and generally do not use SQL.
+           q: What is JWT, and how does it work?
         </div>
     );
 };

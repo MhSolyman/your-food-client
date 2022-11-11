7b8d1f65aeaba5a599a55c1a34338ee3300c2eac
MhSolyman <solymanbijoye.com>
2022-11-11 23:00:13 +0600

    add 3rd q and ans

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,13 +3,17 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-          q:Difference between SQL and NoSQL?
+          q:Difference between SQL and NoSQL? <br />
           ans:SQL is the programming language used to interface with relational databases. <br />
            (Relational databases model data as records in rows and tables with logical links between them). <br />
            NoSQL is a class of DBMs that are non-relational and generally do not use SQL.
-           q: What is JWT, and how does it work?
+           q: What is JWT, and how does it work? <br />
            ans: JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting <br />
             information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.
+            <br />
+           q: What is the difference between javascript and NodeJS?
+           a
+
         </div>
     );
 };
